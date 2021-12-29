090a143fe2562934f5e6ae2493d6446463ae0b77
Nirmal Purohit <purohi65@outlook.com>
2021-12-29 18:16:16 -0500

    "Adding support for updating record"
    Problem:
     - New work item
    
    Solution:
     - Converted JSON file data into python class to access the data more
       easily
    
    Note:
     - This commit missed the documentHolder class

--- InvestCalc.py
+++ InvestCalc.py
@@ -1,8 +1,15 @@
 import argparse
+import sys
+import json
+from documentHolder import documentHolder
 
-def parseArgVal(args):
-    if ( args.rrsp or args.tfsa) and not (args.view or args.update):
-        print("You need to choose an action to perform")
+def extractFromJson(jsonFile):
+    with open(jsonFile) as inputFile:
+            jsonData = json.load(inputFile)
+            inputData = documentHolder(**jsonData)
+
+    return inputData
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -10,8 +17,17 @@ if __name__ == "__main__":
     parser.add_argument('-r', '--rrsp', help="Perform actions on RRSP investments", action="store_true")
     parser.add_argument('-t', '--tfsa', help="Perform actions on TFSA investments", action="store_true")
     parser.add_argument('-v', '--view', help="View the investment calculations and records for the choosen type", action="store_true")
-    parser.add_argument('-u', '--update', help="Update the record for the chosen type")
+    parser.add_argument('-u', '--update', type=str, help="Update the record for the chosen type")
 
     args = parser.parse_args()
 
-    parseArgVal(args)
+    if ( args.rrsp or args.tfsa) and not (args.view or args.update):
+        print("Error parsing the argument! \nYou need to choose an action to perform. Check help (-h/--help) for the options")
+        sys.exit(1)
+
+    if args.update:
+        if not args.update.lower().endswith('.json'):
+            print("Incorrect file type!\nonly JSON file is supported")
+            sys.exit(1)
+        
+        inputData = extractFromJson(args.update)
