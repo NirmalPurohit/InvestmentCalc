287690aa491c8775ee57aa3c987ae07fe4f5f3a1
Nirmal Purohit <purohi65@outlook.com>
2022-01-08 13:29:26 -0500

    "Fixing edge case"
    Problem:
     - When there are no documents, the update operation was failing
    
    Solution:
     - Adding if cluase to hanle emply document error for update operation
    
    Note:
     -

--- MongodbIO.py
+++ MongodbIO.py
@@ -8,22 +8,25 @@ class HandleDBActions:
         self.first_transaction = self.investmentCollection.find_one()
 
     def updateRecord(self, data):
-        for key in data.keys():
-            if (isinstance(data[key], str) and not "".__eq__(data[key])) or (isinstance(data[key], float) and data[key] != 0.0):
-                result = self.investmentCollection.update_one({"first_transaction": self.first_transaction["first_transaction"]}, \
-                    {"$set": {key: data[key]}})
-            elif isinstance(data[key], list):
-                for doc in data[key]:
-                    if self.investmentCollection.find_one({"contribution.from" : doc["start"]}):
-                        result = self.investmentCollection.update_one({"contribution.from": doc["start"]}, \
-                              {"$set": {"contribution.$": doc}})
-                    elif self.investmentCollection.find_one({"contribution.to" : doc["end"]}):
-                        result = self.investmentCollection.update_one({"contribution.to": doc["end"]}, \
-                              {"$set": {"contribution.$": doc}})
-                    elif self.investmentCollection.find_one({"contribution.amount" : doc["amount"]}):
-                        result = self.investmentCollection.update_one({"contribution.amount": doc["amount"]}, \
-                              {"$set": {"contribution.$": doc}})
-                    else:
-                        result = self.investmentCollection.update_one({"first_transaction": self.first_transaction["first_transaction"]}, \
-                              {"$push": {"contribution": doc}})
+        if self.first_transaction is None:
+            result = self.investmentCollection.insert_one(data)
+        else:
+            for key in data.keys():
+                if (isinstance(data[key], str) and not "".__eq__(data[key])) or (isinstance(data[key], float) and data[key] != 0.0):
+                    result = self.investmentCollection.update_one({"first_transaction": self.first_transaction["first_transaction"]}, \
+                        {"$set": {key: data[key]}})
+                elif isinstance(data[key], list):
+                    for doc in data[key]:
+                        if self.investmentCollection.find_one({"contribution.start" : doc["start"]}):
+                            result = self.investmentCollection.update_one({"contribution.start": doc["start"]}, \
+                                {"$set": {"contribution.$": doc}})
+                        elif self.investmentCollection.find_one({"contribution.end" : doc["end"]}):
+                            result = self.investmentCollection.update_one({"contribution.end": doc["end"]}, \
+                                {"$set": {"contribution.$": doc}})
+                        elif self.investmentCollection.find_one({"contribution.amount" : doc["amount"]}):
+                            result = self.investmentCollection.update_one({"contribution.amount": doc["amount"]}, \
+                                {"$set": {"contribution.$": doc}})
+                        else:
+                            result = self.investmentCollection.update_one({"first_transaction": self.first_transaction["first_transaction"]}, \
+                                {"$push": {"contribution": doc}})
         return result
